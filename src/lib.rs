2b7606691f3a76bff2dd9af870cdeb8b463308e6
monnoroch <monnoroch@gmail.com>
2015-06-26 16:38:54 +0300

    solved task 1

--- src/lib.rs
+++ src/lib.rs
@@ -1,1 +1,18 @@
 #![allow(dead_code)]
+
+mod task1 {
+	fn sum_multiples_3_or_5_below(below: u64) -> u64 {
+		let mut s = 0;
+		for i in 1..below {
+			if i % 5 == 0 || i % 3 == 0 {
+				s += i;
+			}
+		}
+		s
+	}
+
+	#[test]
+	fn test() {
+		assert_eq!(sum_multiples_3_or_5_below(1000), 233168);
+	}
+}
