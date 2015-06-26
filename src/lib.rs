dffe65ca4d3d4b1cbd7693db02d3bae83a5e9c4e
monnoroch <monnoroch@gmail.com>
2015-06-26 16:39:07 +0300

    solved task 2

--- src/lib.rs
+++ src/lib.rs
@@ -16,3 +16,26 @@ mod task1 {
 		assert_eq!(sum_multiples_3_or_5_below(1000), 233168);
 	}
 }
+
+mod task2 {
+	fn sum_even_fibs_below(below: u64) -> u64 {
+		let mut s = 0;
+		let mut v1;
+		let mut v2 = 1;
+		let mut val = 1;
+		while val <= below {
+			if val % 2 == 0 {
+				s += val;
+			}
+			v1 = v2;
+			v2 = val;
+			val = v1 + v2;
+		}
+		s
+	}
+
+	#[test]
+	fn test() {
+		assert_eq!(sum_even_fibs_below(4000000), 4613732);
+	}
+}
